faa4184333a876595a8f19d3aa5a6171c4114682
Michael Jewett <michael.jewett@sonraisecurity.com>
2020-11-17 13:07:58 -0400

    remorked the directories of my bots

---
+++ aws/delete_vpc/bot.py
@@ -0,0 +1,23 @@
+import sonrai.platform.aws.arn
+
+import logging
+
+def run(ctx):
+    # Delete AWS VPC
+    vpc_client = ctx.get_client().get('iam')
+
+    data = None
+    user_name = None
+    access_key_id = None
+
+    data = ctx.get_policy_evidence()
+    metadata_list = data.get('metadata')
+
+    for metadata in metadata_list:
+        if 'accessKey.userName:' in metadata:
+            user_name = metadata.split(":")[1
+            ]
+            access_key_id = data.get('name')
+
+    logging.info('deleting accesskey: {} for user: {}'.format(access_key_id, user_name))
+    iam_client.delete_access_key(UserName=user_name, AccessKeyId=access_key_id)

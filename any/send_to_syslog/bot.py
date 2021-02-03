67fe49fd15104db8b2670fd3f5c9447b2bb40ed1
Michael Jewett <michael.jewett@sonraisecurity.com>
2021-02-03 10:45:46 -0400

    Test #2

--- any/send_to_syslog/bot.py
+++ any/send_to_syslog/bot.py
@@ -1,35 +1,125 @@
 import logging
 import sonrai.platform.aws.arn
+from datetime import datetime
+import socket
+import json
+import os
+import jwt
 
 log = logging.getLogger()
 
+class syslogHandler:
+    #_TICKET_VIEW_PATH = 'App/TicketDetails?'
+
+
+    def __init__(self):
+        self.syslog_host = os.environ.get('SYSLOGHOST', '159.2.178.229')
+        self.syslogheader_hostname = os.getenv('SYSLOGHEADERHOSTNAME', 'sonraialerts-lambdafunction')
+        self.syslog_port = int(os.getenv('SYSLOGPORT', 514))
+        self.sonraiMessageFormat = os.getenv("MESSAGEFORMAT", "json")
+        self.policyEvidenceMaxLength = int( os.getenv('POLICYEVIDENCEMAXLENGTH', 1000))
+        self._ENV_KEY = 'https://sonraisecurity.com/env'
+        self._TICKET_URL_FMT = 'https://{}.sonraisecurity.com/App/TicketDetails?srn={}'
+
+
+    def syslogDestinationSendMessage(self, msg):
+        syslogheader = datetime.now().strftime( "%b %d %H:%M:%S") + " " + self.syslogheader_hostname
+        sonraiSyslogMessage = syslogheader + " " + msg
+
+        # Send UDP - will eventually hit limits with this when using JSON
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as send_syslog:
+            send_syslog.sendto(sonraiSyslogMessage.encode('utf-8'), (self.syslog_host, self.syslog_port))
+
+    def build_ticket_link(self, client, srn):
+        token = client._token.get()
+        decoded=jwt.decode(token, verify=False)
+        env = decoded[self._ENV_KEY]
+        if not env:
+            raise ValueError("No env present in token")
+        if env == 'dev':
+            sub = 'dev.de'
+        elif env == 'stage':
+            sub = 'stage.s'
+        elif env == 'crc':
+            sub = 'crc'
+        else:
+            raise ValueError("Unsupported env: {}".format(env))
+        return self._TICKET_URL_FMT.format(sub, srn)
+
+    def get_policy_title(self, client):
+        #grab the title of the policy from the ticket
+        pass
+
+
+    def process_alert(self, context):
+        #get ticket data
+        alert = context.config['data']['ticket']
+        # get the ticket's SRN
+        TicketSRN = alert['srn']
+        #generate the link to the ticket
+        client = context.graphql_client()
+        node_link = self.build_ticket_link(client, TicketSRN)
+        #policy_title = self.get_policy_title(client)
+
+        if alert['severityNumeric'] is None:
+            severity = "n/a"
+        else:
+            severity = str(alert['severityNumeric'])
+
+        syslogPayload = ""
+
+        if self.sonraiMessageFormat == "text":
+            syslogPayload = (" [sonrai-ticketAlert]  " +
+                             "policyName=" + str( alert['policy']['ControlPolicyTitle']) +
+                             "|policyid=" + str( alert['policy']['ControlPolicyId']) +
+                             "|firstSeen" + str(alert['firstSeen']) +
+                             "|lastSeen" + str(alert['lastSeen']) +
+                             "|severity=" + str(severity) +
+                             "|account=" + str(alert['account']) +
+                             "|resourceLabel=" + str(alert['resourceLabel']) +
+                             "|resourceType=" + str(alert['resourceType']) +
+                             "|resourceSRN=" + str(alert['resourceSRN']) +
+                             "|details_url=" + str(node_link) +
+                             "\n")
+        elif self.sonraiMessageFormat == "json":
+            # json to string
+            alertobject = self.alertJSON(alert, node_link, severity)
+            syslogPayload = json.dumps(alertobject)
+
+        self.syslogDestinationSendMessage(syslogPayload)
+
+    # Generate alert json object
+    def alertJSON(self, alert, node_link, severity):
+        if len(json.dumps(
+            alert['evidence']['policyEvidence'])) > self.policyEvidenceMaxLength:
+            evidence = {
+                "policyEvidence": "message too long, refer to Sonrai for full ticket details."}
+        else:
+            evidence = alert['evidence']['policyEvidence']
+
+        sonraiEvent = {
+            #'policyName': alert['policy']['ControlPolicyTitle'],
+            'policyName': alert['policy']['title'],
+            'source': 'Sonrai Security Control Policy',
+            'firstSeen': alert['firstSeen'],
+            'lastSeen': alert['lastSeen'],
+            'severity': severity,
+            'account': alert['account'],
+            'resourceLabel': alert['resourceLabel'],
+            'resourceType': alert['resourceType'],
+            'resourceSRN': alert['resourceSRN'],
+            'custom_details': {
+                'details_url': node_link,
+                'policyEvidence': evidence
+            }
+        }
+
+        data = {
+            "client": "Sonrai Security Platform Alert",
+            'payload': sonraiEvent,
+        }
+        return data
+
 def run(ctx):
-    ticket = ctx.config
-    log.info(ticket)
-    exit(0)
-    for item in ctx.resourceSRN:
-        print (item)
-    exit(0)
-    user_arn = sonrai.platform.aws.arn.parse(ctx.resource_id)
-    user_name = user_arn \
-        .assert_service("iam") \
-        .assert_type("user") \
-        .name
-    # Delete login profile
-    log.info("[{}] Deleting login profile, if any".format(user_arn))
-    try:
-        client.delete_login_profile(UserName=user_name,)
-    except botocore.exceptions.ClientError as e:
-        error = e.response.get("Error")
-        # AWS User may not have a login profile already
-        if not error or error.get("Code") != "NoSuchEntity":
-            raise e
-
-    # Delete access keys
-    for response in _responses(client.list_access_keys, UserName=user_name):
-        for access_key in response['AccessKeyMetadata']:
-            access_key_id = access_key['AccessKeyId']
-            log.info("[{}] Deleting access key: {}".format(user_arn, access_key_id))
-            client.delete_access_key(UserName=user_name, AccessKeyId=access_key_id)
-
-    log.info("[{}] User disabled".format(user_arn))
+    handler = syslogHandler()
+    handler.process_alert(ctx)

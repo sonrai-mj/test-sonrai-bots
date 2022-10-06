fcb710b24389eda9d327416b6b5346ca541ce8c7
Michael Jewett <michael.jewett@sonraisecurity.com>
2022-10-06 16:26:00 -0300

    creating a dummybot

---
+++ dummyBot/bot.py
@@ -0,0 +1,16 @@
+import logging
+from sonrai import gql_loader
+import sys
+import re
+import time
+from datetime import datetime, timedelta
+
+def run(ctx):
+    #CONSTANTS
+    _maxAccountsToAdd = 10
+    # create gql_loader queries
+    gql = gql_loader.queries()
+
+
+    # Get the ticket data from the context
+    logging.info (ctx)

bc1bf09a185c36cfaf5f24c9208a98b437d31f25
Michael Jewett <michael.jewett@sonraisecurity.com>
2022-10-07 15:52:52 -0300

    another change

--- dummyBot/bot.py
+++ dummyBot/bot.py
@@ -7,10 +7,6 @@ from datetime import datetime, timedelta
 
 def run(ctx):
     #CONSTANTS
-    _maxAccountsToAdd = 10
-    # create gql_loader queries
-    # gql = gql_loader.queries()
-
 
     # Get the ticket data from the context
     logging.info (ctx)

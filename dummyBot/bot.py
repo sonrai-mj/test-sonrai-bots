6a30919a05327f92e6f43271c925cad64d915bf0
Michael Jewett <michael.jewett@sonraisecurity.com>
2022-10-12 16:03:56 -0300

    another change

--- dummyBot/bot.py
+++ dummyBot/bot.py
@@ -4,4 +4,8 @@ def run(ctx):
     #CONSTANTS
 
     # Get the ticket data from the context
-    logging.info (ctx)
+    #logging.info (ctx)
+    for x in ctx.config.get('data'):
+        logging.info(x)
+        for y in ctx.config.get('data').get(x):
+            print(y)

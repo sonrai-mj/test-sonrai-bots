53a2ab8886b23eef0eda5a17d4614e6f79f88fad
Michael Jewett <michael.jewett@sonraisecurity.com>
2022-11-08 16:05:50 -0400

    another change

---
+++ dummyBot/add_comment.py
@@ -0,0 +1,19 @@
+def add_ticket_comment(ctx, body):
+   mutation_add_comment = ('''mutation addTicketComment($ticketSrn: String!, $body: String!, $createdBy: String!) {
+  CreateTicketComment(
+    input: {body: $body, ticketSrn: $ticketSrn, createdBy: $createdBy}
+  ) {
+    resourceId
+    srn
+    createdBy
+    createdDate
+    body
+  }
+}''' )
+   ticket_srn = ctx['srn']
+   org_name = ctx['orgName']
+   user_srn = 'srn:' + org_name + '::SonraiUser/bot_user'
+   variables = ('{"ticketSrn": "' + ticket_srn + '", "body": "' + body + '", "createdBy": "' + user_srn + '" }')
+   graphql_client.query(mutation_add_comment, variables)
+
+   
--- dummyBot/bot.py
+++ dummyBot/bot.py
@@ -1,6 +1,8 @@
 import logging
 import json
 
+import add_comment
+
 def run(ctx):
     #CONSTANTS
 
@@ -10,3 +12,5 @@ def run(ctx):
     print (json.dumps(w, indent=4))
     logging.info("testing log")
     logging.info (json.dumps(w))
+    comment = "Adding the following accounts to the collector \n123123234\n23234234\n234234234"
+    ac = add_comment.add_ticket_comment(ctx, comment)
